118b14ded1244ed44138b8500dc8b8272d047413
이선호 <dltjsgho94@gmail.com>
2021-12-12 19:07:07 +0900

    Add : 외부 dispatch  jsonplaceholder 사용하는중

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,4 @@
 //모든 액션들
 export { addSubscriber, removeSubscriber } from './subscribers/action';
 export { addView } from './views/actions';
+export { fetchComments } from './comments/action';
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware } from 'redux';
 // import SubscribersReducer from './subscribers/reducer';
 import rootReducer from './rootReducer';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const middleware = [logger];
+const middleware = [logger, thunk];
 // const store = createStore(SubscribersReducer);
 const store = createStore(
   rootReducer,
