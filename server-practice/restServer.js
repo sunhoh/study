84a47dc43cce18489158aa9f557bb201b085780c
sunhoh <dltjsgho94@gmail.com>
2023-04-04 11:42:46 +0900

    Add: react-router-example

--- server-practice/restServer.js
+++ server-practice/restServer.js
@@ -6,31 +6,23 @@ const users = {}; // 데이터 저장용
 
 http
   .createServer(async (req, res) => {
-    console.log(">>>>", req, res);
     try {
       if (req.method === "GET") {
         if (req.url === "/") {
-          const data = await fs.readFile(
-            path.join(__dirname, "restFront.html")
-          );
-          res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+          let data = await fs.readFile(path.join(__dirname, "restFront.html"));
+          res.writeHead(200, { "Content-Type": "text/html charset=utf-8" });
           return res.end(data);
-        } else if (req.url === "/about") {
+        }
+        if (req.url === "/about") {
           const data = await fs.readFile(path.join(__dirname, "about.html"));
-          res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-          return res.end(data);
-        } else if (req.url === "/users") {
+          res.writeHead(200, { "Content-Type": "text/html charset=utf-8" });
+          return res.end(JSON.stringify(data));
+        }
+        if (req.url === "users") {
           res.writeHead(200, {
-            "Content-Type": "application/json; charset=utf-8",
+            "Content-Type": "application/json charset=utf-8",
           });
           return res.end(JSON.stringify(users));
-        }
-        // /도 /about도 /users도 아니면
-        try {
-          const data = await fs.readFile(path.join(__dirname, req.url));
-          return res.end(data);
-        } catch (err) {
-          // 주소에 해당하는 라우트를 못 찾았다는 404 Not Found error 발생
         }
       } else if (req.method === "POST") {
         if (req.url === "/user") {
@@ -49,36 +41,11 @@ http
             res.end("등록 성공");
           });
         }
-      } else if (req.method === "PUT") {
-        if (req.url.startsWith("/user/")) {
-          const key = req.url.split("/")[2];
-          let body = "";
-          req.on("data", (data) => {
-            body += data;
-          });
-          return req.on("end", () => {
-            console.log("PUT 본문(Body):", body);
-            users[key] = JSON.parse(body).name;
-            res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-            return res.end(JSON.stringify(users));
-          });
-        }
-      } else if (req.method === "DELETE") {
-        if (req.url.startsWith("/user/")) {
-          const key = req.url.split("/")[2];
-          delete users[key];
-          res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-          return res.end(JSON.stringify(users));
-        }
       }
-      res.writeHead(404);
-      return res.end("NOT FOUND");
-    } catch (err) {
-      console.error(err);
-      res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
-      res.end(err.message);
+    } catch (error) {
+      console.log(error);
     }
   })
   .listen(8082, () => {
-    console.log("8082번 포트에서 서버 대기 중입니다");
+    console.log("8082번 포트에서 서버 대기중");
   });
